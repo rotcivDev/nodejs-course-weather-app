122668eba163a0099f230f6e334a068c01337bd1
rotcivdev <vsallesa@gmail.com>
2020-12-11 18:54:53 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const request = require('postman-request');
+const chalk = require('chalk');
+
+const url = "http://api.weatherstack.com/current?access_key=ddc54550988e3e8c401df1417e33c74b&query=Duque%20de%20Caxias";
+
+const forecast = msg => console.log(chalk.bold(msg));
+
+const getWeatherData = async () => {
+  return request({
+    url: url,
+    json: true
+  }, async (error, response) => {
+    let currentData= response.body.current;
+    let locationData = response.body.location;
+
+    forecast(`
+    Its ${locationData.localtime.split(' ').reverse().join(' ')} at ${locationData.region}, ${locationData.country}.
+    Current: ${currentData.temperature}ºC 
+    Feels like: ${currentData.feelslike}ºC
+    Description: ${currentData.weather_descriptions}
+    Rain: ${currentData.cloudcover}%
+    Wind: ${currentData.wind_speed} km/h, Direction: ${currentData.wind_dir}`)    
+  });
+}
+
+getWeatherData();
