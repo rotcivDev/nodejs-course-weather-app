4d5ad3f0ede86a4403fa6fa336ccd1cd774b8785
rotcivdev <vsallesa@gmail.com>
2020-12-15 10:44:16 -0300

    geolocation api integration + error handling

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const request = require('postman-request');
 const chalk = require('chalk');
 
-const url = "http://api.weatherstack.com/current?access_key=ddc54550988e3e8c401df1417e33c74b&query=Duque%20de%20Caxias";
+const weatherUrl = `http://api.weatherstack.com/current?access_key=ddc54550988e3e8c401df1417e33c74b&query=-22.7896,-43.3099`;
+const geolocationUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Duque%20de%20Caxias.json?access_token=pk.eyJ1Ijoicm90Y2l2ZGV2IiwiYSI6ImNraWtzbHZxYzBhMG0ydnBkajVwdmd1NXcifQ.bsH5VFrmtBcdT6G-g4Y57w&limit=1";
 
 const forecast = msg => console.log(chalk.bold(msg));
 
-const getWeatherData = async () => {
+const getWeatherData = () => {
   return request({
-    url: url,
+    url: weatherUrl,
     json: true
-  }, async (error, response) => {
+  }, (error, response) => {
+
+    if (error) {
+      forecast('Unable to conect to weather service.');
+      return;
+    }
+
+    if (response.body.error) {
+      forecast(response.body.error.info);
+      return;
+    }
+
     let currentData= response.body.current;
     let locationData = response.body.location;
-    debugger
     forecast(`
     Its ${locationData.localtime.split(' ').reverse().join(' ')} at ${locationData.region}, ${locationData.country}.
     Current: ${currentData.temperature}ºC 
@@ -22,5 +33,27 @@ const getWeatherData = async () => {
     Wind: ${currentData.wind_speed} km/h, Direction: ${currentData.wind_dir}`)    
   });
 }
-
 getWeatherData();
+
+const getForwardGeolocation = () => {  
+  return request({
+    url: geolocationUrl,
+    json: true
+  }, (error, response) => {
+    if (error) {
+      forecast('Unable to conect to geolocation service.')
+      return;
+    }
+
+    if (response.body.message) {
+      forecast(`Location API Error: ${response.body.message}`);
+      return;
+    }
+
+    let forwardGeolocation = response.body.features[0].center;
+    let latitude = forwardGeolocation[1];
+    let longitude = forwardGeolocation[0];
+    forecast(`Mapbox data: Latitude: ${latitude}, Longitude: ${longitude}`)    
+  })
+}
+getForwardGeolocation();
